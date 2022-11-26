8b9d57d1f9341dacac0dd662dae2894f3411ab60
Ashley Montine Sproul <montinesproul@gmail.com>
2022-11-26 10:54:08 -0500

    created timeblocks in html file used moment injs to display current day/date"

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,15 @@
-console.log(moment())
+// variable to create date
+var todaysDate = document.querySelector('#todayIs')
+// function using moment to display current date
+setInterval(function() {
+    todayIs.textContent = "Today is: " + moment().format('dddd, MMMM Do, YYYY')
+})
+
+
+// if time === current time then display color x
+
+
+//if time has past display color y
+
+//if time is in the future display color z
+

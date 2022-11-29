936e4892797d22ec52f0549c4704df325b95f040
Ashley Montine Sproul <montinesproul@gmail.com>
2022-11-28 23:32:25 -0500

    working on changing the color by hour

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,44 @@
 // variable to create date
-var todaysDate = document.querySelector('#todayIs')
+var todaysDate = document.querySelector('#todayIs');
+
 // function using moment to display current date
+
+
+
 setInterval(function() {
-    todayIs.textContent = "Today is: " + moment().format('dddd, MMMM Do, YYYY')
-})
+    var currentHr = moment().hours();
+    var hour = document.querySelector('.hour').textContent;
+    var elements = document.querySelectorAll('.description'); 
+    // var elements = element.length ++;
+    todayIs.textContent = "Today is: " + (moment().format('LLLL'));
+    // elements.forEach(changeColor);
+    for (let i = 0; i < elements.length; i++) {
+    function changeColor() {
+         if (hour < currentHr) {
+            
+            $(this).addClass("past");
+            
+        }
+        if (hour === currentHr) {
+            element.classList.remove("past");
+            element.classList.add("present");
+        }
+        if (hour > currentHr) {
+            element.classList.remove("past");
+            element.classList.remove("present");
+            element.classList.add("future");
+        }
+    };
+} 
+console.log(elements);
+console.log(currentHr);
+console.log(hour);
+}, 5000);
 
 
 // if time === current time then display color x
+
+
 
 
 //if time has past display color y
@@ -17,8 +49,12 @@ setInterval(function() {
 
 
 var oneEvent= localStorage.getItem('task6');
-var EventAtOne = document.getElementById('task6');
-EventAtOne.val(OneEvent);
-document.addEventListener("click", function(oneEvent){});
-Event.preventDefault();
+var eventAtOne = document.getElementById('task6');
+document.addEventListener("click", function(){
+    var key = document.getElementById('task6');
+    var value = eventAtOne.val;
+    localStorage.setItem(key, value);
+    
+});
+// event.preventDefault();
 // document.getElementById("one").addEventListener("click", oneEvent);

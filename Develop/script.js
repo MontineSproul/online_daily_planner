e2a1491ccf0ced88fdd4ea3264ca3e7eb6879f68
Ashley Montine Sproul <montinesproul@gmail.com>
2022-12-02 21:22:16 -0500

    adjusting setInterval variable

--- Develop/script.js
+++ Develop/script.js
@@ -2,24 +2,23 @@
 var todaysDate = document.querySelector('#todayIs');
 
 // function using moment to display current date
-
-
-
-setInterval(function() {
+var displayTime = function() {
     var currentHr = moment().hours();
-    var hour = document.querySelector('.hour').textContent;
+    var hours = document.querySelectorAll('.hour');
     var elements = document.querySelectorAll('.description'); 
     // var elements = element.length ++;
     todayIs.textContent = "Today is: " + (moment().format('LLLL'));
     // elements.forEach(changeColor);
     for (let i = 0; i < elements.length; i++) {
-    function changeColor() {
+    function changeColor(element, i) {
+        var hourEl = hours[i];
+        var hour = hourEl.dataset.hour;
+        console.log(hour, currentHr);
          if (hour < currentHr) {
-            
-            $(this).addClass("past");
-            
+            element.classList.add("past");
+            console.log("less than");
         }
-        if (hour === currentHr) {
+        if (hour == currentHr) {
             element.classList.remove("past");
             element.classList.add("present");
         }
@@ -29,11 +28,15 @@ setInterval(function() {
             element.classList.add("future");
         }
     };
+    changeColor(elements[i], i);
 } 
-console.log(elements);
-console.log(currentHr);
-console.log(hour);
-}, 5000);
+// console.log(elements);
+// console.log(currentHr);
+
+}
+displayTime();
+
+setInterval(displayTime, 60000);
 
 
 // if time === current time then display color x
